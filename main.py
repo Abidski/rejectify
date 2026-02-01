20a76d13e96f976be756293d1b2c9f30a80bd263
Abidski <abijanahmed@gmail.com>
2026-02-01 16:43:52 -0500

    Read emails

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ from dotenv import load_dotenv
 
 from client import EmailClient
 
-load_dotenv()
+_ = load_dotenv()
 
 email = os.getenv("ADDRESS")
 password = os.getenv("PASSWORD")
 server = os.getenv("SERVER")
 
 if __name__ == "__main__":
-    client = EmailClient(email, password, server)
-    client.Connect()
+    try:
+        if email and password and server:
+            client = EmailClient(email, password, server)
+            client.Connect()
+            client.GetEmails()
+        else:
+            print("Env error")
+
+    except Exception as e:
+        print(e)
